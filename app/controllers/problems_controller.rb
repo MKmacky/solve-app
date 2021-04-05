1148cb09e34fc20927abe09b85521a7104e0cf4e
MKmacky <mkazuma529@icloud.com>
2021-04-05 19:12:25 +0900

    相談ルーム作成機能実装

--- app/controllers/problems_controller.rb
+++ app/controllers/problems_controller.rb
@@ -1,8 +1,26 @@
 class ProblemsController < ApplicationController
+
   def index
+    @problems = Problem.all
   end
 
   def new
     @problem = Problem.new
   end
+
+  def create
+    @problem = Problem.new(problem_params)
+    if @problem.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def problem_params
+    params.require(:problem).permit(:title, :content)
+  end
+
 end
--- app/models/problem.rb
+++ app/models/problem.rb
@@ -1,4 +1,7 @@
 class Problem < ApplicationRecord
   has_many :problem_users
   has_many :users, through: :problem_users
+
+  validates :title,   presence: true
+  validates :content, presence: true
 end
