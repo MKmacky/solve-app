e4ca5c960f2fddf73970d4f9d17616f771e0307e
MKmacky <mkazuma529@icloud.com>
2021-04-05 18:02:50 +0900

    年齢選択欄追加

---
+++ app/models/age.rb
@@ -0,0 +1,16 @@
+class Age < ActiveHash::Base
+  self.data = [
+    { id: 0, name: '--' },
+    { id: 1, name: '10歳以下' },
+    { id: 2, name: '10代' },
+    { id: 3, name: '20代' },
+    { id: 4, name: '30代' },
+    { id: 5, name: '40代' },
+    { id: 6, name: '50代' },
+    { id: 7, name: '60歳以上' }
+  ]
+
+  include ActiveHash::Associations
+  has_many :users
+
+  end
