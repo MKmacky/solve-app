f29b544b736eafdf9d04168d52aa771a4e73503a
MKmacky <mkazuma529@icloud.com>
2021-04-07 14:42:39 +0900

    パスワードのバリデーション追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   validates :name,        presence: true
+  validates :password,    format: { with: /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]{8,100}+\z/i }, on: :create
   validates :gender,      presence: true
   validates :age_id,      numericality: { other_than: 0 }
   validates :position_id, numericality: { other_than: 0 }
